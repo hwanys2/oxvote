5ea1b59f806da32b205d5f7499c5ae5b09cb0c6f
hwanys2 <hwanys2na@gmail.com>
2025-09-17 20:49:45 +0900

    Fix Railway deployment: Remove PostgreSQL dependencies, use SQLite only

--- oxvote/settings.py
+++ oxvote/settings.py
@@ -91,12 +91,6 @@ DATABASES = {
     }
 }
 
-# Railway PostgreSQL 설정 (환경변수가 있는 경우)
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
